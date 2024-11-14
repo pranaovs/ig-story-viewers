7bfef52431d818a523d15fc5c2e690aec8f89787
Pranaov <148376495+pranaovs@users.noreply.github.com>
2024-11-14 19:42:04 +0530

    feat: functions to get story list and story viewers

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# ~./.venv/bin/python
+#!./.venv/bin/python
 
 from instagrapi import Client
 import os
 from dotenv import load_dotenv
+from instagrapi.types import Story, UserShort
 
 load_dotenv()
 
@@ -12,8 +13,6 @@ IG_PASSWORD = os.environ.get("IG_PASSWORD")
 IG_CREDENTIAL_PATH = "./ig_settings.json"
 SLEEP_TIME = "600"  # in seconds
 
-cl = Client
-
 
 class Bot:
     _cl = None
@@ -21,8 +20,31 @@ class Bot:
     def __init__(self):
         self._cl = Client()
         if os.path.exists(IG_CREDENTIAL_PATH):
-            self._cl.load_settings(IG_CREDENTIAL_PATH)
+            self._cl.load_settings(IG_CREDENTIAL_PATH)  # type: ignore
             self._cl.login(IG_USERNAME, IG_PASSWORD)
         else:
             self._cl.login(IG_USERNAME, IG_PASSWORD)
-            self._cl.dump_settings(IG_CREDENTIAL_PATH)
+            self._cl.dump_settings(IG_CREDENTIAL_PATH)  # type: ignore
+
+    def get_self_stories(self):
+        # print(self._cl.account_info())
+        story_list: list[Story] = self._cl.user_stories(self._cl.account_info().pk)
+        return story_list
+
+    def get_story_viewers(self):
+        story_list = self.get_self_stories()
+        story_viewers: list[UserShort] = []
+        for story in story_list:
+            story_viewers.append(self._cl.story_viewers(story.pk))
+        return story_viewers
+
+
+def main():
+    goodbot = Bot()
+    print(goodbot.get_story_viewers())
+
+
+if __name__ in "__main__":
+    print("===========\n\n")
+    main()
+    print("\n\n===========")
